3fd6086387913f9d6b6edb13776d755d78e958c8
daws-86ss <arjunmsp123@gmail.com>
2025-09-29 11:39:07 +0530

    variables learning

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+person1=ramesh
+person2=suresh
+
+
+echo "$person1: hi $person2 , how are you"
+echo "$person2: hi $person1 , i'm doing good"
+echo "$person1: what are you learning recently"
+echo "$person2:i'm learning devsceops with aws"
