881e5fa795d57a37601f05a7b0651a036074ba42
daws-86ss <arjunmsp123@gmail.com>
2025-09-29 12:23:42 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+person1=$1
+person2=$2
+
+
+echo "person1:hi person2"
+echo "person2: hi person1"
+echo "person1: oka godava ayyindi"
+echo "person2: am ayyindi person1"
+echo "person1:creicket aade daggara problem ayyindi person2"
+echo "person2: sare am cheddam person1"
