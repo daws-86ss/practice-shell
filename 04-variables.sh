a71f27589e21faf0b391de71ff21b3d08154a155
daws-86ss <arjunmsp123@gmail.com>
2025-09-29 12:35:28 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,9 +5,9 @@ person1=
 person2=
 
 
-echo "person1:hi person2"
-echo "person2: hi person1"
-echo "person1: oka godava ayyindi"
-echo "person2: am ayyindi person1"
-echo "person1:creicket aade daggara problem ayyindi person2"
-echo "person2: sare am cheddam person1"
+echo "$person1:hi $person2"
+echo "$person2: hi $person1"
+echo "$person1: oka godava ayyindi"
+echo "$person2: am ayyindi $person1"
+echo "$person1:creicket aade daggara problem ayyindi $person2"
+echo "$person2: sare am cheddam $person1"
