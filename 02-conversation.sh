ccd555d94161bb4f71a5065bfc00aee580dd0373
daws-86ss <arjunmsp123@gmail.com>
2025-09-29 11:32:06 +0530

    conversation between two people

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "ramesh: hi suresh , how are you"
+echo "suresh: hi ramesh , i'm doing good"
+echo "ramesh: what are you learning recently"
+echo "suresh:i'm learning devsceops with aws"
