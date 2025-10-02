46b7063c88736de727973c96c65f069829a765ed
daws-86ss <arjunmsp123@gmail.com>
2025-10-02 10:48:28 +0530

    abc

---
+++ 11-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID - eq 0]
+    echo "ERROR: please run with root previliges"
+    exit 1
+fi
+
+
+dnf insatll mysql -y
+
+if [ $? - ne 0]
+    echo "ERROR: insatlling mysql is failure"
+    exit 1
+else
+    echo "insatlling my sql"
