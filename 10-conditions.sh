17a998d030721c76597e4e65004202d5e5971a83
daws-86ss <arjunmsp123@gmail.com>
2025-10-02 09:59:31 +0530

    conditions

---
+++ 10-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter the number: "
+read NUMBER
+
+if [ $(($NUMBER % 2)) ]; then
+    echo "given number $NUMBER is even"
+else 
+     echo "given number $NUMBER is odd"
