6277d2d7fe4011cc166af1ce79e06c84b4bb246f
daws-86ss <arjunmsp123@gmail.com>
2025-10-03 08:26:01 +0530

    absd

---
+++ 13-colors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:run the script with root previliges"
+    exit 1
+fi
+
+
+dnf install nginx -y
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 is failure"
+        exit 1
+    else
+        echo "installing $2 is success"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "NGINX"
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install python3 -y
+VALIDATE $? "PYTHON3"
+
